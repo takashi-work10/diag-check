772f53f3743d87f679f32ce4803e8f9496210e24
miura hiroto <sanpuhiroto469@gmail.com>
2025-04-16 15:49:00 +0900

    Simple profile function

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -3,6 +3,9 @@ import NextAuth from "next-auth";
 declare module "next-auth" {
     interface User {
         id: string;
+        name?: string | null;
+        email?: string | null;
+        image?: string | null;
     }
     interface Session {
         user: User;
